d1bb78bd3020eb0377bec8c75ccedc5cd5152a5c
toandev020101 <toandev020101@gmail.com>
2022-09-24 22:22:32 +0700

    update login register forgot-password page

---
+++ assets/js/password.js
@@ -0,0 +1,19 @@
+// let - const
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const passwordBtn = $('.password-btn')
+const passwordInput = $('.password')
+// end let - const
+
+// hide show password
+passwordBtn.onclick = () => {
+	const type = passwordInput.getAttribute('type')
+	if (type == 'password') {
+		passwordInput.setAttribute('type', 'text')
+		passwordBtn.setAttribute('class', 'bx bx-show-alt password-btn')
+	} else {
+		passwordInput.setAttribute('type', 'password')
+		passwordBtn.setAttribute('class', 'bx bx-hide password-btn')
+	}
+}
